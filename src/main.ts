d491f47514fa15883f3a813aefdfd63e69f09f9d
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-11-27 17:19:57 +0700

    fix(deploy)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('The users API description')
@@ -27,7 +28,7 @@ async function bootstrap() {
       persistAuthorization: true,
     },
   });
-
-  await app.listen(3000);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
