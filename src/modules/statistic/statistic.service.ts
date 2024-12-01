e0d9123db79f0b6bda8e7185f32aa8c1d73e2ec5
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-12-01 21:07:56 +0700

    config(deploy)

--- src/modules/statistic/statistic.service.ts
+++ src/modules/statistic/statistic.service.ts
@@ -1,15 +1,19 @@
 import { Booking } from "@modules/booking/entity/booking.entity";
 import { Inject } from "@nestjs/common";
 import { BookingRepository } from "@repositories/booking.repository";
-import { BaseServiceAbstract } from "src/services/base/base.abstract.service";
+import { FlightRepository } from "@repositories/flight.repository";
 
-export class StatisticService extends BaseServiceAbstract<Booking> {
+export class StatisticService {
     constructor(
         @Inject('BOOKING_REPOSITORY') 
-        private readonly bookingRepository: BookingRepository
-    ) {
-        super(bookingRepository);
-    }
+        private readonly bookingRepository: BookingRepository,
+
+        @Inject('FLIGHT_REPOSITORY')
+        private readonly flightRepository: FlightRepository,
+
+        @Inject('TICKET_REPOSITORY')
+        private readonly ticketRepository: FlightRepository,
+    ) {}
 
     
 }
