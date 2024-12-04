5dd54398dd3e6439b53018645b94a4709234d85a
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-12-04 23:06:32 +0700

    fix(cors)

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000', 
-    methods: 'GET, POST, PUT, DELETE', 
+    methods: 'GET, POST, PATCH, DELETE', 
     allowedHeaders: 'Content-Type, Accept',
   });
   const configService = app.get(ConfigService);
