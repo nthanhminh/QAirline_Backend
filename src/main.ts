a93c5334ec4abe808187ad7ca286690956aeadcf
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-12-04 22:49:37 +0700

    fix(cors)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET, POST, PUT, DELETE', 
+    allowedHeaders: 'Content-Type, Accept',
+  });
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('Users API')
