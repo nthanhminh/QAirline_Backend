7db4dad335839836abb2be976a446fb0497d1d4f
ngothanhminh2004 <shinichikudo1423@gmail.com>
2024-12-19 20:45:36 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     origin: ['http://localhost:3000', 'https://qairline-admin-frontend.vercel.app', 'https://qairline-fe.vercel.app'], 
     methods: 'GET, POST, PATCH, DELETE', 
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
   });
   // app.useGlobalInterceptors(new LoggingInterceptor());
   const configService = app.get(ConfigService);
